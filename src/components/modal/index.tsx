020ca29455c3b77cfd1b990e54b4ca22ee3b0ce0
0hyeon <djdjdjk2006@naver.com>
2024-01-15 00:16:13 +0900

    feat: gsap mouse event

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,11 +1,51 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./style.module.css";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import gsap from "gsap";
+const scaleAnimation = {
+  initial: { scale: 0, x: "-50%", y: "-50%" },
+  open: {
+    scale: 1,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
+  },
+  close: {
+    scale: 0,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+  },
+};
 export default function Modal({ modal, projects }: any) {
+  const { active, index } = modal;
+  const container = useRef(null);
+  useEffect(() => {
+    const moveContainerX = gsap.quickTo(container.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    const moveContainerY = gsap.quickTo(container.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      moveContainerX(clientX);
+      moveContainerY(clientY);
+    });
+  }, []);
   return (
-    <div className={styles.modalContainer}>
-      <div className={styles.modalSlider}>
+    <motion.div
+      ref={container}
+      variants={scaleAnimation}
+      initial={"initial"}
+      animate={active ? "open" : "close"}
+      className={styles.modalContainer}
+    >
+      <div style={{ top: index * -100 + "%" }} className={styles.modalSlider}>
         {projects.map((el: any, idx: number) => {
           const { src, color } = el;
           return (
@@ -24,6 +64,6 @@ export default function Modal({ modal, projects }: any) {
           );
         })}
       </div>
-    </div>
+    </motion.div>
   );
 }
